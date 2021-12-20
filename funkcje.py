dd62a1f08152b33fbe273484a0bd3721bee8607f
astaciwa <01169353@pw.edu.pl>
2021-12-20 10:59:00 +0100

    v2

--- funkcje.py
+++ funkcje.py
@@ -1,14 +1,18 @@
 import os
 
 def pierwsza(nazwa):
+    assert isinstance(nazwa, str), "Podany parametr nie jest napisem!"
+    assert len(nazwa) != 0, "Podaj niepustą nazwę pliku!"
+    if os.path.exists(os.path.abspath(nazwa)) == False:
+        raise FileExistsError("Nie znaleziono katalogu!")
     slownik = {}
     files = os.listdir(os.path.abspath(nazwa))
     for file in files:
-        if os.path.isdir(file):
-            continue
-        else:
+        if os.path.isfile(nazwa + "\\" + file):
             sciezka = os.path.splitext(file)
             rozmiar = os.path.getsize(os.path.abspath(nazwa + "\\" + file))
             slownik[sciezka[1]] = {sciezka[0]: rozmiar}
+        else:
+            continue
 
     return slownik

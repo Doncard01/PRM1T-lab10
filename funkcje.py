ba5344fac8cbc1e496dcc8ab66f7e5f30b30c75d
astaciwa <01169353@pw.edu.pl>
2021-12-20 11:33:13 +0100

    działa zad1

--- funkcje.py
+++ funkcje.py
@@ -5,13 +5,18 @@ def pierwsza(nazwa):
     assert len(nazwa) != 0, "Podaj niepustą nazwę pliku!"
     if os.path.exists(os.path.abspath(nazwa)) == False:
         raise FileExistsError("Nie znaleziono katalogu!")
+
     slownik = {}
+
     files = os.listdir(os.path.abspath(nazwa))
     for file in files:
         if os.path.isfile(nazwa + "\\" + file):
             sciezka = os.path.splitext(file)
             rozmiar = os.path.getsize(os.path.abspath(nazwa + "\\" + file))
-            slownik[sciezka[1]] = {sciezka[0]: rozmiar}
+            if sciezka[1] in slownik.keys():
+                slownik[sciezka[1]].update({sciezka[0]: rozmiar})
+            else:
+                slownik[sciezka[1]] = {sciezka[0]: rozmiar}
         else:
             continue
 
